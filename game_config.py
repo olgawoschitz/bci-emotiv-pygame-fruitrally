5c8da54f77ca24869abe5663d2f3a1fb8af3daf0
Olga Zharikova <o.zharikova@campus.tu-berlin.de>
2021-02-27 17:57:27 +0100

    updated

--- game_config.py
+++ game_config.py
@@ -8,7 +8,3 @@ class GameConfig:
         "client_secret": "cnMe6DTjgKLBR1UAtHquYKMEnLHY7WG5KJfZCB5zFf3UhTOfbFKbalIBMl3OOgoNmfQpvW03n8KfGkafUyjo154PyyCsXOXnt5hec0U95dLsd4ej85rGOijFgATzT4p1",
         "debit": 100
     }
-
-    def __init__(self, screen_width=1024, screen_height=768):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
